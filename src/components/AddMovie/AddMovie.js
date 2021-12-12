cf61700e4b82aced7c1c65610730073f86c33aa3
Bennett Smrdel <smrdelb@gmail.com>
2021-12-12 09:11:45 -0600

    genre dropdown menu now works, and returns a genre id

--- src/components/AddMovie/AddMovie.js
+++ src/components/AddMovie/AddMovie.js
@@ -31,6 +31,11 @@ function AddMovie() {
         setDescription(event);
     }
 
+    const handleGenreChange = (event) => {
+        setGenre(event);
+        console.log(event);
+    }
+
     // Handles form being submitted
         // Save values for all inputs into a reducer
         // Clear input forms
@@ -60,21 +65,21 @@ function AddMovie() {
                     onChange={(event) => {handleDescriptionChange(event.target.value)}} ></textarea>
                 <div>
                     <label htmlFor="genres">Choose a genre</label>
-                    <select name="genres">
+                    <select name="genres" onChange={(event) => {handleGenreChange(event.target.value)}}>
                         <option></option>
-                        <option>Adventure</option>
-                        <option>Animated</option>
-                        <option>Biographical</option>
-                        <option>Comedy</option>
-                        <option>Disaster</option>
-                        <option>Drama</option>
-                        <option>Epic</option>
-                        <option>Fantasy</option>
-                        <option>Musical</option>
-                        <option>Romantic</option>
-                        <option>Science Fiction</option>
-                        <option>Space-Opera</option>
-                        <option>Superhero</option>
+                        <option value="1">Adventure</option>
+                        <option value="2">Animated</option>
+                        <option value="3">Biographical</option>
+                        <option value="4">Comedy</option>
+                        <option value="5">Disaster</option>
+                        <option value="6">Drama</option>
+                        <option value="7">Epic</option>
+                        <option value="8">Fantasy</option>
+                        <option value="9">Musical</option>
+                        <option value="10">Romantic</option>
+                        <option value="11">Science Fiction</option>
+                        <option value="12">Space-Opera</option>
+                        <option value="13">Superhero</option>
                     </select>
                 </div>
                 <div>
